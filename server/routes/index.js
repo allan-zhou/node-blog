f1d0ae6e8eaa18fd69f78accc83aca480bcf38d1
周佳良 <izhoujialiang@qq.com>
2017-08-14 13:36:18 +0800

    server routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'login' });
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'register' });
 });
 
 module.exports = router;
